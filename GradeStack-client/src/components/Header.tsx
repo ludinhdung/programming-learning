f66dc9c6640be1fb9cf3da8695209dcb0f178da9
huynhgocnhan <huynhngocnhan29@gmail.com>
2025-02-09 10:55:01 +0700

    base frontend

---
+++ GradeStack-client/src/components/Header.tsx
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+import { Dialog, DialogPanel, PopoverGroup } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const Header: React.FC = () => {
+  const navItems = [
+    { name: "Topics", href: "#" },
+    { name: "Path", href: "#" },
+    { name: "Features", href: "#" },
+    { name: "Blog", href: "#" },
+    { name: "Teams", href: "#" },
+  ];
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <>
+      <header className="bg-gray-900/95">
+        <nav
+          aria-label="Global"
+          className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-4 flex-wrap"
+        >
+          <div className="flex-1 flex justify-start">
+            <PopoverGroup className="flex lg:flex lg:gap-x-4 space-x-4">
+              {navItems.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-sm/6 font-semibold text-gray-300"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </PopoverGroup>
+          </div>
+          <div className="flex justify-center flex-1">
+            {/*logo*/}
+            <a aria-label="Laracasts" className="cursor-pointer" href="/">
+              <svg width="40" viewBox="0 0 100 90" fill="none">
+                <rect x="40" width="20" height="30" fill="white"></rect>
+                <rect x="20" y="20" width="20" height="10" fill="white"></rect>
+                <rect
+                  x="10"
+                  y="50"
+                  width="20"
+                  height="20"
+                  fill="#459FFB"
+                ></rect>
+                <rect y="70" width="20" height="20" fill="#459FFB"></rect>
+                <rect x="70" y="50" width="20" height="20" fill="white"></rect>
+                <rect x="40" y="70" width="60" height="20" fill="white"></rect>
+                <rect x="20" y="30" width="20" height="10" fill="white"></rect>
+                <rect x="60" y="20" width="20" height="10" fill="white"></rect>
+                <rect x="60" y="30" width="20" height="10" fill="white"></rect>
+                <rect x="60" y="40" width="20" height="10" fill="white"></rect>
+              </svg>
+            </a>
+          </div>
+
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center space-x-4">
+            {/*Search icon*/}
+            <button className="flex items-center justify-center rounded-md bg-gray-700 p-2 text-white hover:bg-gray-600 focus:outline-none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="h-5 w-5"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                <path d="M21 21l-6 -6" />
+              </svg>
+            </button>
+
+            <a
+              href="$"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Get Started For Free
+            </a>
+            <a href="#" className="text-sm/6 font-semibold text-gray-300">
+              Sign in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </nav>
+
+        <Dialog
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+          className="lg:hidden"
+        >
+          <div className="fixed inset-0 z-10" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="py-6 ">
+                  <div className="flex justify-center">
+                    <button className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="h-5 w-5"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                        <path d="M21 21l-6 -6" />
+                      </svg>
+                    </button>
+                  </div>
+                  <a
+                    href="#"
+                    className="text-center bg-indigo-600 -mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Get Started For Free
+                  </a>
+                  <a
+                    href="#"
+                    className="text-center -mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Sign in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </header>
+    </>
+  );
+};
+export default Header;
