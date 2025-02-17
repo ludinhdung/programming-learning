9493455676e092efec2f0d70b688a5054a339365
huynhgocnhan <huynhngocnhan29@gmail.com>
2025-02-17 13:09:14 +0700

    update direct components

--- GradeStack-client/src/App.tsx
+++ GradeStack-client/src/App.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
+import CourseStudy from "./pages/CourseStudyPage/CourseStudy";
 import NotFound from "./pages/NotFound";
 
 const App: React.FC = () => {
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />{" "}
+        <Route path="/course-study" element={<CourseStudy />} />
       </Routes>
     </Router>
   );
---
+++ GradeStack-client/src/components/CourseStudyPageComponents/TopBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TopBar: React.FC = () => {
+   return (
+      <div>
+         
+      </div>
+   );
+};
+export default TopBar;
