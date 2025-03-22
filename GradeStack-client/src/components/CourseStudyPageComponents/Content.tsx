e59f6b75388d438173c640e68b143fe99def92e3
huynhgocnhan <huynhngocnhan29@gmail.com>
2025-03-22 21:42:36 +0700

    update studyCourse + commentSection

--- GradeStack-client/src/components/CourseStudyPageComponents/Content.tsx
+++
@@ -1,8 +0,0 @@
-const Content: React.FC = () => { 
-   return (
-      <div className="bg-slate-600 rounded-md h-[700px]">
-            <h1 className="text-white text-center text-2xl font-semibold">This is Video or Code space</h1>
-      </div>
-   );
-};
-export default Content;
